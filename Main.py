d16afe9e2f5c1879b52b787cf0817178e01bca90
DELL PRO <goluwafemi2009@gmail.com>
2024-11-22 14:20:10 +0100

    Access API keys, API url from nasa for website

---
+++ Main.py
@@ -0,0 +1,27 @@
+import requests
+import streamlit as st
+
+# Prepare API key and API url
+api_key = "ZFKgoV36lGH79JlZvdu3exISofzon8Iipu2vJcq7"
+url = ("https://api.nasa.gov/planetary/apod?"\
+       "api_key=ZFKgoV36lGH79JlZvdu3exISofzon8Iipu2vJcq7")
+
+# Get the request data
+request = requests.get(url)
+# Set a request data as a dictionary
+data = request.json()
+
+# Extract the image title, url and explanation
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+# Download the image
+image_filepath = "img.jpg"
+response = requests.get(image_url)
+with open(image_filepath, "wb") as file:
+       file.write(response.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
